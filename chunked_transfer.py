dee9976c2d54a70de72c083549ec1e74bc9b98bf
sreekaratla <sreekar.atla@gmail.com>
2020-07-19 12:31:49 +0530

    initial commit

--- chunked_transfer.py
+++ chunked_transfer.py
@@ -23,13 +23,14 @@ Usage examples:
 
 """
 
-import httplib2
 import os
 import random
 import sys
 import time
+import urllib.request
+import urllib.error
 
-from apiclient.discovery import build as discovery_build
+from googleapiclient.discovery import build as discovery_build
 from apiclient.errors import HttpError
 from apiclient.http import MediaFileUpload
 from apiclient.http import MediaIoBaseDownload
@@ -37,7 +38,7 @@ from json import dumps as json_dumps
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage as CredentialStorage
 from oauth2client.tools import run_flow as run_oauth2
-
+from urllib.error import HTTPError
 
 # CLIENT_SECRETS_FILE, name of a file containing the OAuth 2.0 information for
 # this application, including client_id and client_secret. You can acquire an
@@ -77,7 +78,7 @@ with information from the APIs Console
                                    CLIENT_SECRETS_FILE))
 
 # Retry transport and file IO errors.
-RETRYABLE_ERRORS = (httplib2.HttpLib2Error, IOError)
+RETRYABLE_ERRORS = (HTTPError, IOError)
 
 # Number of times to retry failed downloads.
 NUM_RETRIES = 5
@@ -90,7 +91,7 @@ DEFAULT_MIMETYPE = 'application/octet-stream'
 
 
 def get_authenticated_service(scope):
-  print 'Authenticating...'
+  print('Authenticating...')
   flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE, scope=scope,
                                  message=MISSING_CLIENT_SECRETS_MESSAGE)
 
@@ -99,14 +100,14 @@ def get_authenticated_service(scope):
   if credentials is None or credentials.invalid:
     credentials = run_oauth2(flow, credential_storage)
 
-  print 'Constructing Google Cloud Storage service...'
+  print('Constructing Google Cloud Storage service...')
   http = credentials.authorize(httplib2.Http())
   return discovery_build('storage', 'v1', http=http)
 
 
 def handle_progressless_iter(error, progressless_iters):
   if progressless_iters > NUM_RETRIES:
-    print 'Failed to make progress for too many consecutive iterations.'
+    print('Failed to make progress for too many consecutive iterations.')
     raise error
 
   sleeptime = random.random() * (2**progressless_iters)
@@ -127,15 +128,15 @@ def upload(argv):
 
   service = get_authenticated_service(RW_SCOPE)
 
-  print 'Building upload request...'
+  print('Building upload request...')
   media = MediaFileUpload(filename, chunksize=CHUNKSIZE, resumable=True)
   if not media.mimetype():
     media = MediaFileUpload(filename, DEFAULT_MIMETYPE, resumable=True)
   request = service.objects().insert(bucket=bucket_name, name=object_name,
                                      media_body=media)
 
-  print 'Uploading file: %s to bucket: %s object: %s ' % (filename, bucket_name,
-                                                          object_name)
+  print('Uploading file: %s to bucket: %s object: %s ' % (filename, bucket_name,
+                                                          object_name))
 
   progressless_iters = 0
   response = None
@@ -145,11 +146,11 @@ def upload(argv):
       progress, response = request.next_chunk()
       if progress:
         print_with_carriage_return('Upload %d%%' % (100 * progress.progress()))
-    except HttpError, err:
+    except urllib.error.HTTPError as e:
       error = err
       if err.resp.status < 500:
         raise
-    except RETRYABLE_ERRORS, err:
+    except RETRYABLE_ERRORS as err:
       error = err
 
     if error:
@@ -158,10 +159,10 @@ def upload(argv):
     else:
       progressless_iters = 0
 
-  print '\nUpload complete!'
+  print('\nUpload complete!')
 
-  print 'Uploaded Object:'
-  print json_dumps(response, indent=2)
+  print('Uploaded Object:')
+  print(json_dumps(response, indent=2))
 
 
 def download(argv):
@@ -171,15 +172,15 @@ def download(argv):
 
   service = get_authenticated_service(RO_SCOPE)
 
-  print 'Building download request...'
+  print('Building download request...')
   f = file(filename, 'w')
   request = service.objects().get_media(bucket=bucket_name,
                                         object=object_name)
   media = MediaIoBaseDownload(f, request, chunksize=CHUNKSIZE)
 
-  print 'Downloading bucket: %s object: %s to file: %s' % (bucket_name,
+  print('Downloading bucket: %s object: %s to file: %s' % (bucket_name,
                                                            object_name,
-                                                           filename)
+                                                           filename))
 
   progressless_iters = 0
   done = False
@@ -190,11 +191,11 @@ def download(argv):
       if progress:
         print_with_carriage_return(
             'Download %d%%.' % int(progress.progress() * 100))
-    except HttpError, err:
+    except HttpError as err:
       error = err
       if err.resp.status < 500:
         raise
-    except RETRYABLE_ERRORS, err:
+    except RETRYABLE_ERRORS as err:
       error = err
 
     if error:
@@ -203,16 +204,16 @@ def download(argv):
     else:
       progressless_iters = 0
 
-  print '\nDownload complete!'
+  print('\nDownload complete!')
 
 
 if __name__ == '__main__':
   if len(sys.argv) < 3:
-    print 'Too few arguments.'
-    print USAGE
+    print('Too few arguments.')
+    print(USAGE)
   if sys.argv[2].startswith('gs://'):
     upload(sys.argv)
   elif sys.argv[1].startswith('gs://'):
     download(sys.argv)
   else:
-    print USAGE
+    print(USAGE)
